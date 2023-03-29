fa7d9284268699a1a7a09a08cfb7d2d71a5534bd
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2023-03-29 02:44:28 +0200

    config: support forcing type for macro values

--- internal/config/macros.go
+++ internal/config/macros.go
@@ -2,12 +2,16 @@ package config
 
 import (
 	"fmt"
+	"reflect"
 
+	"github.com/rafaelmartins/synth-datagen/internal/convert"
+	"github.com/rafaelmartins/synth-datagen/internal/ctypes"
 	"gopkg.in/yaml.v3"
 )
 
 type ConfigMacro struct {
 	Identifier string      `yaml:"-"`
+	Type       string      `yaml:"type"`
 	Value      interface{} `yaml:"value"`
 	Hex        bool        `yaml:"hex"`
 }
@@ -40,6 +44,15 @@ func (c *ConfigMacros) UnmarshalYAML(value *yaml.Node) error {
 			} else {
 				if err := cnt.Decode(m); err != nil {
 					return err
+				}
+				if m.Type != "" {
+					if ctypes.ValidateType(reflect.TypeOf(m.Value)) {
+						value, err := convert.Scalar(m.Value, m.Type)
+						if err != nil {
+							return err
+						}
+						m.Value = value
+					}
 				}
 			}
 			*c = append(*c, m)
